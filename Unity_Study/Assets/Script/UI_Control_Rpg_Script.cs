214d6a1c98738a9c13ed6e0aa4df31fad9a1fd05
KCheolHyeon <ch970314@naver.com>
2024-03-08 16:12:32 +0900

    RPG스타일의 UI 에셋 추가

---
+++ Unity_Study/Assets/Script/UI_Control_Rpg_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Control_Rpg : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
